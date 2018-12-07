71808512698139e79fbd292151c61729bc419b70
artlinedesign <programmierung@artline-design.at>
2018-12-07 12:56:04 +0100

    New index
    - adds new admin menu with submenu
    - uncomments old stuff & includes

--- publicationmanager.php
+++ publicationmanager.php
@@ -81,113 +81,142 @@ function run_publicationmanager() {
 }
 run_publicationmanager();
 
-/**
-* Adds Admin Menu
-*
-*
-* Start function init
-*
-*/
+
+//  #### NEW 
+//  ###
+//	###
 
 
 
+// Add Plugin into Admin Menu
 add_action('admin_menu', 'publicationmanager');
 
-add_action( 'admin_enqueue_scripts', 'wp_enqueue_media' );
-
-function load_thickbox() {
-    wp_enqueue_script('thickbox');
-    wp_enqueue_style('thickbox');
-}
-
-add_action( 'admin_enqueue_scripts', 'load_thickbox' );
-
-
 function publicationmanager(){
-	add_menu_page('Publication Manager', 'Publication Manager', 'manage_options', 'publicationmanager', 'init' );
-	add_submenu_page('publicationmanager', 'Publication Manager', 'Publication Manager', 'manage_options', 'publicationmanager' );
-	add_submenu_page('publicationmanager', 'Autoren', 'Autoren', 'manage_options', 'autoren', 'showAuthors' );
-	add_submenu_page('publicationmanager', 'Beiträge', 'Beiträge', 'manage_options', 'beitraege', 'showArticles' );
-    add_submenu_page('publicationmanager', 'Bücher', 'Bücher', 'manage_options', 'buecher', 'showBooks' );
-    add_submenu_page('publicationmanager', 'Publikationen', 'Publikationen', 'manage_options', 'publikationen', 'showPublications' );
-	add_submenu_page('publicationmanager', 'Verläge', 'Verläge', 'manage_options', 'verlaege', 'showPublisher' );
-	add_submenu_page('publicationmanager', 'Erstellen', 'Erstellen', 'manage_options', 'create', 'create' );
 
-}
+	add_menu_page('CM', 'CM', 'manage_options', 'cm', 'init' );
 
-require plugin_dir_path( __FILE__ ) . 'admin/create/model.php';
-
-function init(){
-	createDatabase();
-    require plugin_dir_path( __FILE__ ) . 'admin/landing.phtml';
-}
-
-function createDatabase() {
-	register_activation_hook( __FILE__, 'createCustomDatabase' );
-	require plugin_dir_path( __FILE__ ) . 'includes/class-create-database.php';
-	DatabaseCreator::createCustomDatabase();
-}
-
-require plugin_dir_path( __FILE__ ) . 'includes/class-addfields.php';
-function showAuthors() {
-	require plugin_dir_path( __FILE__ ) . 'admin/authors/view.phtml';
-}
-
-function showPublisher() {
-    require plugin_dir_path( __FILE__ ) . 'admin/verlage/view.phtml';
+	add_submenu_page('cm', 'Autoren', 'Autoren', 'manage_options', 'edit.php?post_type=authors', NULL );
+	add_submenu_page('cm', 'Bücher', 'Bücher', 'manage_options', 'edit.php?post_type=books', NULL );
+	add_submenu_page('cm', 'Publikationen', 'Publikationen', 'manage_options', 'edit.php?post_type=publications', NULL );
+	add_submenu_page('cm', 'Beiträge', 'Beiträge', 'manage_options', 'edit.php?post_type=articles', NULL );
+	add_submenu_page('cm', 'Verläge', 'Verläge', 'manage_options', 'edit.php?post_type=publisher', NULL );
 }
 
 
-function showArticles() {
-    require plugin_dir_path( __FILE__ ) . 'admin/articles/view.phtml';
-}
-
-function showBooks() {
-    require plugin_dir_path( __FILE__ ) . 'admin/books/view.phtml';
-}
-
-
-function create() {
-    require plugin_dir_path( __FILE__ ) . 'admin/create/create.phtml';
-}
-
-
-function showPublications() {
-	require plugin_dir_path( __FILE__ ) . 'admin/publications/view.phtml';
-}
-
-
-function showAdminErrorMessage($errMessage) {
-    $class = 'notice notice-error';
-    $message = __( $errMessage, 'sample-text-domain' );
-
-    printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
-}
-add_action( 'showAdminError', 'showAdminErrorMessage' );
-
-function showAdminSuccessMessage($msg) {
-    $class = 'notice notice-success';
-    $message = __( $msg, 'sample-text-domain' );
-
-    printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
-}
-add_action( 'showAdminSuccess', 'showAdminSuccessMessage' );
-
-
-add_shortcode('publications', 'publicationsShortcode');
-
-require plugin_dir_path( __FILE__ ) . 'public/shortcode/publications.phtml';
-
-
-add_shortcode('articles', 'articlesShortcode');
-
-require plugin_dir_path( __FILE__ ) . 'public/shortcode/articles.phtml';
-
-
-add_shortcode('books', 'booksShortcode');
-
-require plugin_dir_path( __FILE__ ) . 'public/shortcode/books.phtml';
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * ### CUSTOM ###
+ * Begins execution of the custom classes.
+ *
+ */
+
+// add_action('admin_menu', 'publicationmanager');
+
+// add_action( 'admin_enqueue_scripts', 'wp_enqueue_media' );
+
+// function load_thickbox() {
+//     wp_enqueue_script('thickbox');
+//     wp_enqueue_style('thickbox');
+// }
+
+// add_action( 'admin_enqueue_scripts', 'load_thickbox' );
+
+
+// function publicationmanager(){
+// 	add_menu_page('Publication Manager', 'Publication Manager', 'manage_options', 'publicationmanager', 'init' );
+// 	add_submenu_page('publicationmanager', 'Publication Manager', 'Publication Manager', 'manage_options', 'publicationmanager' );
+// 	add_submenu_page('publicationmanager', 'Autoren', 'Autoren', 'manage_options', 'autoren', 'showAuthors' );
+// 	add_submenu_page('publicationmanager', 'Beiträge', 'Beiträge', 'manage_options', 'beitraege', 'showArticles' );
+//     add_submenu_page('publicationmanager', 'Bücher', 'Bücher', 'manage_options', 'buecher', 'showBooks' );
+//     add_submenu_page('publicationmanager', 'Publikationen', 'Publikationen', 'manage_options', 'publikationen', 'showPublications' );
+// 	add_submenu_page('publicationmanager', 'Verläge', 'Verläge', 'manage_options', 'verlaege', 'showPublisher' );
+// 	add_submenu_page('publicationmanager', 'Erstellen', 'Erstellen', 'manage_options', 'create', 'create' );
+// }
+
+// require plugin_dir_path( __FILE__ ) . 'admin/create/model.php';
+
+// function init(){
+// 	createDatabase();
+//     require plugin_dir_path( __FILE__ ) . 'admin/landing.phtml';
+// }
+
+// function createDatabase() {
+// 	register_activation_hook( __FILE__, 'createCustomDatabase' );
+// 	require plugin_dir_path( __FILE__ ) . 'includes/class-create-database.php';
+// 	DatabaseCreator::createCustomDatabase();
+// }
+
+// require plugin_dir_path( __FILE__ ) . 'includes/class-addfields.php';
+// function showAuthors() {
+// 	require plugin_dir_path( __FILE__ ) . 'admin/authors/view.phtml';
+// }
+
+// function showPublisher() {
+//     require plugin_dir_path( __FILE__ ) . 'admin/verlage/view.phtml';
+// }
+
+
+// function showArticles() {
+//     require plugin_dir_path( __FILE__ ) . 'admin/articles/view.phtml';
+// }
+
+// function showBooks() {
+//     require plugin_dir_path( __FILE__ ) . 'admin/books/view.phtml';
+// }
+
+
+// function create() {
+//     require plugin_dir_path( __FILE__ ) . 'admin/create/create.phtml';
+// }
+
+
+// function showPublications() {
+// 	require plugin_dir_path( __FILE__ ) . 'admin/publications/view.phtml';
+// }
+
+
+// function showAdminErrorMessage($errMessage) {
+//     $class = 'notice notice-error';
+//     $message = __( $errMessage, 'sample-text-domain' );
+
+//     printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+// }
+// add_action( 'showAdminError', 'showAdminErrorMessage' );
+
+// function showAdminSuccessMessage($msg) {
+//     $class = 'notice notice-success';
+//     $message = __( $msg, 'sample-text-domain' );
+
+//     printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+// }
+// add_action( 'showAdminSuccess', 'showAdminSuccessMessage' );
+
+
+// add_shortcode('publications', 'publicationsShortcode');
+
+// require plugin_dir_path( __FILE__ ) . 'public/shortcode/publications.phtml';
+
+
+// add_shortcode('articles', 'articlesShortcode');
+
+// require plugin_dir_path( __FILE__ ) . 'public/shortcode/articles.phtml';
+
+
+// add_shortcode('books', 'booksShortcode');
+
+// require plugin_dir_path( __FILE__ ) . 'public/shortcode/books.phtml';
 ?>
